919065377d29043a35a41c61699c411b8a0462b3
Stefan de Maré <stefan.demare@outlook.com>
2017-06-21 10:02:16 +0200

    First changes

---
+++ Garage_2_0/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Garage_2_0.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+    }
+}
