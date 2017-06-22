faa3b6259815f9c7c1175cce04e40c36eb89f0f0
Stefan de Maré <stefan.demare@outlook.com>
2017-06-22 11:09:18 +0200

    Textcorrections

--- Garage_2_0/Models/Member.cs
+++ Garage_2_0/Models/Member.cs
@@ -14,6 +14,7 @@ namespace Garage_2_0.Models
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Range(18, 100)]
         public int Age { get; set; }
         [Display(Name = "Phone Number")]
         public string Phone { get; set; }
